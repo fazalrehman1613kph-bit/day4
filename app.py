69c1958322796fec9cfe117799b2413339df40b8
Fazal Rehman <fazalrehman161.3kph@gmail.com>
2025-08-31 23:21:03 -0700

    my git to show

---
+++ app.py
@@ -0,0 +1,6 @@
+noun = input("What is your noun:")
+verb = input("What is your verb:")
+adjective = input("What is your adjective:")
+story = f"you {noun.upper()} is most of the time in {verb.replace("verb","b")} and working so {adjective}"
+print("\nHere's your story: ")
+print(story)
